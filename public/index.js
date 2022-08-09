a397351048f64740d58bc13aae5e1626d321b63b
denis <88676144+sherstnew@users.noreply.github.com>
2022-08-09 13:40:51 +0300

    work in progress

--- public/index.js
+++ public/index.js
@@ -1,10 +1,41 @@
 const more_btn = document.querySelector('.more-btn')
+const req_btn = document.querySelector('.req-btn')
+const vk = document.querySelector('.req-vk')
+const req_conf = document.querySelector('.req-conf')
+const req_err = document.querySelector('.req-err')
 
-more_btn.addEventListener('click', ()=>{
+more_btn.addEventListener('click', () => {
     const scr = setInterval(()=>{
         window.scrollBy(0, 10)
-        if (scrollY > 580) {
+        if (scrollY > 1000) {
             clearInterval(scr)
         }
     }, 1)
+})
+
+req_btn.addEventListener('click', () => {
+    const scr = setInterval(()=>{
+        window.scrollBy(0, 10)
+        if (scrollY > 2100) {
+            clearInterval(scr)
+        }
+    }, 1)
+})
+
+req_conf.addEventListener('click', () => {
+    if (vk.value != '' && vk.value.includes('https://vk.com/')) {
+        const xhr = new XMLHttpRequest()
+        let body = {
+            vk: '',
+        }
+        body.vk = vk.value
+        body = JSON.stringify(body)
+        xhr.open('POST', '/api', true)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.send(body)
+        window.scroll(0, 0)
+        location.reload()
+    } else {
+        req_err.innerHTML = 'Некорректно введена ссылка'
+    }
 })
--- server.js
+++ server.js
@@ -1,21 +1,38 @@
+const Database = require("@replit/database")
 const express = require('express')
 const favicon = require('serve-favicon')
 const path = require('path')
 
+const db = new Database()
 const app = express()
 const port = 3000
 
+let body = ''
+let c = 0
+
 app.use(express.static('public'));
 app.use("/public", express.static('public'));
+app.use(express.json())
 
 app.get('/', (req, res) => {
   app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-  res.sendFile('/home/runner/edenory/index.html')
+  res.sendFile('C:/Users/Денис/projects/edenory/index.html')
 })
-app.post('/', (req, res) => {
-    console.log(req);
+
+app.get('/list', (req, res) => {
+  db.list().then(keys => {
+    res.send(typeof(keys))
+  });
+})
+
+app.post('/api', (req, res) => {
+    body = req.body
+    c++
+    db.set(c, body)
     res.send('ok')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Starting on http://127.0.0.1:${port}`)
