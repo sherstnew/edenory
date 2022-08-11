ab325b21a8803ad4b25b69112e96318936dac0ff
denis <88676144+sherstnew@users.noreply.github.com>
2022-08-11 20:27:15 +0000

    fixes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const port = 3000
 
 let body = ''
 let c = 0
+let list = ''
 
 app.use(express.static('public'));
 app.use("/public", express.static('public'));
@@ -16,20 +17,27 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
   app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-  res.sendFile('C:/Users/Денис/projects/edenory/index.html')
+  res.sendFile('/home/runner/edenory/index.html')
 })
 
 app.get('/list', (req, res) => {
   db.list().then(keys => {
-    res.send(typeof(keys))
+    for (let key in keys) {
+      db.get(key).then(key => {
+        list = list + key + ','
+      })
+    }
+    res.send(list)
   });
 })
 
 app.post('/api', (req, res) => {
-    body = req.body
-    c++
-    db.set(c, body)
-    res.send('ok')
+  vk = req.body.vk
+  c++
+  db.set(c, vk).then(() => {
+    console.log(c, vk)
+  })
+  res.send('ok')
 })
 
 
